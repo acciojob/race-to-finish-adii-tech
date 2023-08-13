dcaeab4a907ee1399098513033d6316b988d4840
Aditya Kadam <79622885+adii-tech@users.noreply.github.com>
2023-08-13 22:57:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+
+function createPromises() {
+let randomTime = Math.random()*4000;
+let randomPromise = new Promise((resolve,reject)=>{
+	setTimeout(resolve(randomTime),randomTime);
+});
+	for (let index = 0; index < 5; index++) {
+	 promises.push(randomPromise);
+		
+	}
+}
+createPromises();
+Promise.any(promises).then(ele=>{
+	document.getElementByID('output').innerHTML = ele;
+})
+
